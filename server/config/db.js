d37b434c5dbdf6dae5d140d2fcb8f799483da62f
KProgramming29 <krunalprajapati2903@gmail.com>
2022-10-29 00:47:12 -0400

    midterm

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  "URI":"mongodb+srv://Krunal:jN5RzMHQG23vI5xZ@clustert.ehriwip.mongodb.net/book229?retryWrites=true&w=majority"
+};
